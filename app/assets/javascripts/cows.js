c019d1d853ae6f633a6a7346edf26f5e8a5ccc41
Craig Kuriger <craig.kuriger@crssoftware.co.nz>
2015-12-02 15:26:41 +1300

    unneeded jquery/bootstrapdelete

---
+++ app/assets/javascripts/cows.js
@@ -0,0 +1,15 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+// $(document).ready(function(){
+//
+//     $('.featurette1').show();
+//
+//     $('ul li a').click(function(){
+//         $cow = $(this).attr("class");
+//         $('.featurette').hide();
+//         $('.featurette' + $cow.slice(-1)).show();
+//
+//     });
+// });
