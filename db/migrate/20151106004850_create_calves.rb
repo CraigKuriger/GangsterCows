948639de77f4965dda23739ba5ebfa0ddbe80451
Craig Kuriger <craig.kuriger@crssoftware.co.nz>
2015-12-03 16:27:53 +1300

    add country stats

--- db/migrate/20151106004850_create_calves.rb
+++ db/migrate/20151106004850_create_calves.rb
@@ -2,6 +2,9 @@ class CreateCalves < ActiveRecord::Migration
   def change
     create_table :calves do |t|
       t.string :name
+      t.integer :age
+      t.float :money
+      t.string :country
       t.references :cow, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20151106004850) do
 
   create_table "calves", force: :cascade do |t|
     t.string   "name"
+    t.integer  "age"
+    t.float    "money"
+    t.string   "country"
     t.integer  "cow_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
